852f3ed4d4fc44dae53012e91373312f7177ae69
Abdullah Naim <aanaim96@gmail.com>
2024-08-17 19:53:10 -0400

    Add files via upload

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,75 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFav ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleFav">Toggle Favorites</button>
+    <button @click="toggleDetails">{{ detailShow ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailShow">
+      <li>Phone: {{ phoneNumber }}</li>
+      <li>Email: {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress',
+  //   'isFav',
+  // ],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFav: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
+    },
+  },
+  emits: [
+    'toggle-fav'
+  ],
+  // emits: {
+  //   'toggle-fav': function(id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("Id is missing")
+  //       return false;
+  //     }
+  //   },
+  // },
+  data() {
+    return {
+      detailShow: false,
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailShow = !this.detailShow
+      this.phoneNumber
+    },
+    toggleFav() {
+      this.$emit('toggle-fav', this.id);
+    }
+  }
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" for="name" v-model="enteredName">
+    </div>
+    
+    <div>
+      <label for="Phone">Phone</label>
+      <input type="tel" for="name" v-model="enteredPhone"> 
+    </div>
+    
+    <div>
+      <label for="">Email</label>
+      <input type="email" for="name" v-model="enteredEmail">
+    </div>
+
+    <div>
+      <button>Add Contact</button>
+    </div>
+ </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-contact'],
+  data() {
+    return {
+         enteredName: '',
+         enteredPhone: '',
+         enteredEmail: '',
+      }
+  },
+  methods: {
+    submitData() {
+      this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail)
+      console.log('pressed add new friend');
+    }
+  }
+}
+</script>
